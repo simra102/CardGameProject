b88e4ab64b194f7e6cc8f5e844ec0c5b2f7b22c2
Arshraj Kaur <kauarshr@sheridancollege.ca>
2025-02-10 20:50:59 -0500

    added rank and suit

---
+++ RummyGame/src/rummygame/Card.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rummygame;
+
+/**
+ *
+ * @author HP
+ */
+public class Card {
+    private String rank;
+    private String suit;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
+
