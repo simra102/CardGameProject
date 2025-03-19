af674793ce52e35050f77f32a1db9120d1391c7c
Arshraj Kaur <kauarshr@sheridancollege.ca>
2025-03-18 23:23:05 -0400

    updated

--- RummyGame/src/rummygame/Hand.java
+++ RummyGame/src/rummygame/Hand.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rummygame;
-import java.util.ArrayList;
-import java.util.List;
-/**
- *
- * @author HP
- */
-public class Hand {
 
+import java.util.*;
+
+public class Hand {
     private List<Card> cards;
 
     public Hand() {
-        this.cards = new ArrayList<>();
+        cards = new ArrayList<>();
     }
 
     public void addCard(Card card) {
         cards.add(card);
     }
 
-    public void removeCard(Card card) {
-        cards.remove(card);
+    public boolean removeCard(Card card) {
+        return cards.remove(card);
     }
 
     public List<Card> getCards() {
@@ -33,4 +25,3 @@ public class Hand {
         System.out.println("Hand: " + cards);
     }
 }
-
