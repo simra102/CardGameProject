74779e2ec804219bba7ac2ff1ec537db084c35ff
Arshraj Kaur <kauarshr@sheridancollege.ca>
2025-03-18 23:23:21 -0400

    updated

--- RummyGame/src/rummygame/RummyGame.java
+++ RummyGame/src/rummygame/RummyGame.java
@@ -1,23 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author HP
- */
 package rummygame;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class RummyGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Rummy!\n");
-        
+
         // Create a game with two players
         Game game = new Game(Arrays.asList("Player 1", "Player 2"));
-        
+
         // Start the game
         game.startGame();
     }
