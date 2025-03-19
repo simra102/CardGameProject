96e51e91778d17e3e5f3cd1fb391d9a64c52af44
Arshraj Kaur <kauarshr@sheridancollege.ca>
2025-03-18 23:20:53 -0400

    updated

--- RummyGame/src/rummygame/Deck.java
+++ RummyGame/src/rummygame/Deck.java
@@ -1,53 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rummygame;
+
 import java.util.*;
-/**
- *
- * @author HP
- */
+
 public class Deck {
- 
     private List<Card> cards;
+    private Random rand;
 
     public Deck() {
         cards = new ArrayList<>();
+        rand = new Random();
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
         String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
 
-        // Create 52 cards (one of each rank in all suits)
         for (String suit : suits) {
             for (String rank : ranks) {
                 cards.add(new Card(rank, suit));
             }
         }
-        shuffle();
     }
 
-    // Shuffle the deck
     public void shuffle() {
         Collections.shuffle(cards);
     }
 
-    // Draw a card from the top of the deck
     public Card drawCard() {
-        if (!cards.isEmpty()) {
-            return cards.remove(0);
+        if (cards.isEmpty()) {
+            return null;
         }
-        return null; // No more cards left
-    }
-
-    // Check if the deck is empty
-    public boolean isEmpty() {
-        return cards.isEmpty();
-    }
-
-    // Get the number of remaining cards
-    public int remainingCards() {
-        return cards.size();
+        return cards.remove(cards.size() - 1);
     }
 }
-
-
