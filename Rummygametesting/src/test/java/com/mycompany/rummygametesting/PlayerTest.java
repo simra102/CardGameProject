7a3a59947ef82175da75ef50785579f371f774e6
Arshraj Kaur <kauarshr@sheridancollege.ca>
2025-04-09 16:25:28 -0400

    updated according to project3

--- Rummygametesting/src/test/java/com/mycompany/rummygametesting/PlayerTest.java
+++ Rummygametesting/src/test/java/com/mycompany/rummygametesting/PlayerTest.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
+    by arshraj,sirman and kriss */
 package com.mycompany.rummygametesting;
 
 import org.junit.jupiter.api.Test;
